9eb24e4b3615e515252aaadd8ccb5bdfd8dc9482
mahak540 <162689029+mahak540@users.noreply.github.com>
2025-07-15 15:45:49 +0530

    The Number of Employees Which Report to Each Employee
    Table: Employees
    
    +-------------+----------+
    | Column Name | Type     |
    +-------------+----------+
    | employee_id | int      |
    | name        | varchar  |
    | reports_to  | int      |
    | age         | int      |
    +-------------+----------+
    employee_id is the column with unique values for this table.
    This table contains information about the employees and the id of the manager they report to. Some employees do not report to anyone (reports_to is null). 
     
    
    For this problem, we will consider a manager an employee who has at least 1 other employee reporting to them.
    
    Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
    
    Return the result table ordered by employee_id.

---
+++ thenoofemoloyeeswhichreporttoeachemployee.sql
@@ -0,0 +1,5 @@
+select m.Employee_id,m.name ,count(e.reports_to)as reports_count,round(avg(e.age*1.00),0) as average_age
+from Employees e ,Employees m 
+where e.reports_to =m.Employee_id 
+group by  m.Employee_id ,m.name 
+order by  Employee_id asc 
